9c437d1c2c7bc5e8c0d4aa5721d3acfef16b7f1c
Abdul Waheed <waheed032433@gmail.com>
2024-12-18 22:07:00 +0500

    I just update the code

--- script.js
+++ script.js
@@ -1,27 +1,63 @@
-// //Question 1  (alert)
-// alert("Welcome to Js")
-// alert("Learn JS")
+//Chapter 1  (alert)
+//Task 1
+// alert("Hello! Welcome to Javascript Webpage")
 
+//Task 2
+// alert("Error! Please enter a valid password")
 
-// //Question 2 Variable for Strings
+//Task 3
+// alert("Learn JS, \n Learn python")
 
-const myName = ("Abdul Waheed")
-console.log(myName)
+//Question 2 Variable for Strings
+// Question 1
+// const username = ("Abdul Waheed")
+// console.log(myName)
 
-const course = ("Modern Web Development")
-console.log(course)
+// // Question 2
+// const myName = ("Abdul Waheed")
+// console.log(myName)
 
-const subject = ("JavaScript")
-console.log(subject)
+// // Question 3
+// const message = ("Hello World")
+// alert(message)
 
+// //Question 4 
+// var studentName = "John Doe";
+// var studentAge = "15 years old";
+// var studentCourse = "Modern Web Development";
 
-// //Question 3 Variable for Numbers
+// alert("Student Name: " + studentName);
+// alert("Student Age: " + studentAge);
+// alert("Student Course: " + studentCourse);
 
-const marks = 90
-console.log(marks)
+//Question 5
+// var food = "pizza\npizz\npiz\npi\np"
+// alert(food)
 
-const age = 28
-console.log(age)
+// Question 6 
+// let message = "my email address is "
+// let email = "waheed032433@gmail.com"
+// alert(message+email)
 
-const salary = 50000
-console.log(salary)
+//Question 7 
+// let message1 = " I am trying to learn from the book"
+// let  book = "“A smarter way to learn JavaScript”."
+
+// alert(message1+book)
+
+//Question 8
+// var message2 = "Yah! I can write HTML content through Javascript"
+// console.log(message2)
+
+// //Question 9 
+// var message3 = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+// alert(message3)
+
+// //Chapter 3 Variable for Numbers
+
+// //Question 1
+
+// var age = "I am 15 year old"
+// alert(age)
+
+//Question 2 
