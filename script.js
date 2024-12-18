a308ad4e798f2d4a0cfa435f2f8f9b83a1e775c2
Abdul Waheed <waheed032433@gmail.com>
2024-12-18 09:41:18 +0500

    Add project files

---
+++ script.js
@@ -0,0 +1,27 @@
+// //Question 1  (alert)
+// alert("Welcome to Js")
+// alert("Learn JS")
+
+
+// //Question 2 Variable for Strings
+
+const myName = ("Abdul Waheed")
+console.log(myName)
+
+const course = ("Modern Web Development")
+console.log(course)
+
+const subject = ("JavaScript")
+console.log(subject)
+
+
+// //Question 3 Variable for Numbers
+
+const marks = 90
+console.log(marks)
+
+const age = 28
+console.log(age)
+
+const salary = 50000
+console.log(salary)
